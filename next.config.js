b074f97db3a45bd08e08c1c72f962cd22dd0ed29
Timothy Dake <spaco67@gmail.com>
2024-12-11 23:55:51 +0100

    feat: add delegates page with analytics and charts

--- next.config.js
+++ next.config.js
@@ -1,20 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     domains: ['lwteensministrytrainingportal.org'],
   },
-};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
